ced1073930cde7449b263688effafa94db427d2f
Sai34Charan <saicharangopisetty23@gmail.com>
2025-05-11 08:42:21 +0530

    Change in Signup

--- src/container/SignUpContainer.jsx
+++ src/container/SignUpContainer.jsx
@@ -1,21 +1,27 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Signup from "../pages/Signup/Signup";
-const SignUpContainer = () => {
+const SignUpContainer = ({onClick}) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
 
   const handleSignUp = (e) => {
     e.preventDefault();
-    console.log({ email, password });
+    console.log({ name, email, password, confirmpassword });
   }
 
   return (
     <Signup
+      name ={name}
       email={email}
       password={password}
+      confirmpassword={confirmpassword}
       onEmailChange={(e) => setEmail(e.target.value)}
       onPasswordChange={(e) => setPassword(e.target.value)}
-      onSignUp={handleSignUp}
+      onConfirmingPassword={(e) => setConfirmPassword(e.target.value)}
+      onSignUp ={handleSignUp}
+      onLoginClick={onClick}
     />
   );
 };
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,37 +1,65 @@
 import React from "react";
 import "./Signup.css"; // Link to the external CSS file
 
-const Signup = () => {
+const Signup = ({
+  name,
+  email,
+  password,
+  confirmpassword,
+  onEmailChange,
+  onPasswordChange,
+  onConfirmingPassword,
+  onSignUp,
+  onLoginClick,
+}) => {
   return (
-    <div className="login-container">
-      <div className="content">
-        <div className="SignUp">
-          <h3>Create an Account</h3>
-          <p>Fill in the information to create your account</p>
-          <form>
-            <label>Name</label>
-            <input placeholder="Enter your name" required />
-            <label>Email</label>
-            <input type="email" placeholder="Enter your email" required />
-            <label>Password</label>
-            <input type="password" placeholder="Enter your password" required />
-            <label>Confirm Password</label>
-            <input
-              type="password"
-              placeholder="Confirm your password"
-              required
-            />
-            <button type="submit" className="SignUp-button">
-              Sign Up
-            </button>
-          </form>
-          <p className="Victory">
-            Already have an account? <a href="#LoginPage">Login</a>
-          </p>
-        </div>
-      </div>
-    </div>
+    <div className="signup"> 
+    <h3>Sign Up</h3>
+    <p>Fill the information to create your account</p>
+    <form onSubmit={onSignUp}>
+      <label>Name</label>
+      <input
+        type="name"
+        value={name}
+        placeholder="Enter your Name"
+        required
+      />
+      <label>Email</label>
+      <input
+        type="email"
+        value={email}
+        onChange={onEmailChange}
+        placeholder="Enter your email"
+        required
+      />
+      <label>Password</label>
+      <input
+        type="password"
+        value={password}
+        onChange={onPasswordChange}
+        placeholder="Enter your password"
+        required
+      />
+      <label>Confirm Password</label>
+      <input
+        type="password"
+        value={confirmpassword}
+        onChange={onConfirmingPassword}
+        placeholder="Confirming your password"
+        required
+      />
+      <button type="submit" className="signup-button">
+        Signup
+      </button>
+    </form>
+    <p className="loginagain">
+      Already have an account?{" "}
+      <a onClick={onLoginClick} style={{ cursor: "pointer" }}>
+        Login
+      </a>
+    </p>
+  </div>
   );
 };
-
 export default Signup;
+      
